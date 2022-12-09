8976d5aa316dc8678f6b262af585ed2135ca91af
Olivier <hello@olivierplessis.fr>
2022-12-09 16:54:43 +0100

    feat(): first app version

--- lib/common/domain/state/favorite/favorite_list_state.freezed.dart
+++ lib/common/domain/state/favorite/favorite_list_state.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'favorite_list_state.dart';
 
@@ -368,6 +368,7 @@ class _$_Loaded implements _Loaded {
   final List<SingleBook> _singleBooks;
   @override
   List<SingleBook> get singleBooks {
+    if (_singleBooks is EqualUnmodifiableListView) return _singleBooks;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_singleBooks);
   }
@@ -525,6 +526,7 @@ class _$_Empty implements _Empty {
   final List<SingleBook> _singleBooks;
   @override
   List<SingleBook> get singleBooks {
+    if (_singleBooks is EqualUnmodifiableListView) return _singleBooks;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_singleBooks);
   }
